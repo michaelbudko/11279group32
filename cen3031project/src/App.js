cb596f7a27b364047e19f98e8bb76ffd998c82be
eychen2 <echen963@gmail.com>
2021-10-14 10:05:39 -0400

    attempt at starts of query tool

--- cen3031project/src/App.js
+++ cen3031project/src/App.js
@@ -1,11 +1,26 @@
-import React from 'react';
+import React, {useState,useEffect} from 'react'; 
 
-function App()
+
+const App=() =>
 {
+    const[search, setSearch]=useState("");
+    const[query,setQuery]=useState('');
+
+    const updateSearch = e =>{
+        setSearch(e.target.value);
+        console.log(search);
+    }
+    const getSearch = e =>{
+        e.preventDefault();
+        setQuery(search);
+    }
+    useEffect(()=>{ 
+        console.log("effect has been run")
+    }, [query]);
     return(
         <div className="App">
-            <form className="search-form">
-                <input className="search-bar" type="text" placeholder="Search address/ZIP"/>
+            <form onSubmit={getSearch} className="search-form">
+                <input className="search-bar" type="text" placeholder="Search address/ZIP" value={search} onChange={updateSearch} />
                 <button className="search-button" type="submit">Search</button>
             </form>
         </div>
